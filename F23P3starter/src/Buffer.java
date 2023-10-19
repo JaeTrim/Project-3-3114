8c2d091e183ea82d522d06863e4df1367cd926d2
Jae Trimboli <jaetrim@vt.edu>
2023-10-19 16:47:13 -0400

    update buffer

--- F23P3starter/src/Buffer.java
+++ F23P3starter/src/Buffer.java
@@ -1,35 +1,88 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+/**
+ * Buffer Class
+ * 
+ * @author Jae Trimboli (jaetrim)
+ * @author Mohammad Mian (mohammadm21)
+ * @version 2023-10-19
+ */
 public class Buffer {
 
     private int index;
-    private byte dirty;
+    private int dirty;
     private byte[] arr;
+    private RandomAccessFile file;
 
+    /**
+     * Buffer Constuctor
+     * 
+     * @param index
+     *            of the byte array
+     * @param file
+     *            being read and written
+     * @throws IOException
+     */
     public Buffer(int index, RandomAccessFile file) throws IOException {
+        this.file = file;
         this.index = index;
         arr = new byte[4096];
-        read(file);
+        read();
     }
 
 
-    public void read(RandomAccessFile file) throws IOException {
+    /**
+     * Reads the buffer
+     * 
+     * @throws IOException
+     */
+    public void read() throws IOException {
         file.seek(index);
         file.read(arr, index, 4096);
     }
-    
-    public void write() {
-        
+
+
+    /**
+     * Writes the buffer
+     * 
+     * @param newArr
+     * @throws IOException
+     */
+    public void write(byte[] newArr) throws IOException {
+        file.seek(index);
+        file.write(newArr);
+        setDirty(1);
+        arr = newArr;
     }
 
 
-    public byte getDirty() {
+    /**
+     * Gets the array
+     * 
+     * @return byte array
+     */
+    public byte[] getArr() {
+        return arr;
+    }
+
+
+    /**
+     * Gets the dirty byte
+     * 
+     * @return
+     */
+    public int getDirty() {
         return dirty;
     }
 
 
-    public void setDirty(byte val) {
+    /**
+     * Sets the dirty byte
+     * 
+     * @param val
+     */
+    public void setDirty(int val) {
         dirty = val;
     }
 
